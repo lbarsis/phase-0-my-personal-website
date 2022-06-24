d03f3002faa8614814b811091ff047e980966970
Levi Barsis <lbarsis@gmail.com>
2022-06-23 17:53:56 -0700

    Added java function to add comments. Need to create additional functions to allow user to input information and post it to site.

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
   }
-  
-  /* Set the width of the side navigation to 0 */
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-  }
 
-  function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
-  }
-  
-  /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
-  }
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+}
+
+function addComment() {
+  const comment = document.createElement("div");
+  comment.classList.add("comment-block");
+  const commentDiv = document.getElementById("comment-div");
+  commentDiv.append(comment);
+}
 
