7cc6370669a9eb7620b06ca3a58f7f2371dfb19a
Levi Barsis <lbarsis@gmail.com>
2022-06-23 20:50:36 -0700

    Added timestamps to posts. Next step is to create a form for custom posts.

--- index.js
+++ index.js
@@ -19,9 +19,37 @@ function closeNav() {
 }
 
 function addComment() {
-  const comment = document.createElement("div");
-  comment.classList.add("comment-block");
+  const commentBlock = document.createElement("div");
   const commentDiv = document.getElementById("comment-div");
-  commentDiv.append(comment);
+
+  commentBlock.classList.add("comment-block");
+
+  const postedName = document.createElement("p");
+  postedName.classList.add("posted-details");
+  postedName.setAttribute("id", "posted-details-name");
+  postedName.innerHTML = `<i>Name: </i>...`;
+
+  const postedTimeStamp = document.createElement("p");
+  postedTimeStamp.classList.add("posted-details");
+  postedTimeStamp.setAttribute("id", "posted-details-timestamp");
+  postedTimeStamp.textContent = timeStamp();
+
+  const postedDescription = document.createElement("p");
+  postedDescription.classList.add("posted-details");
+  postedDescription.setAttribute("id", "posted-details-description");
+  postedDescription.innerHTML = `Description: `;
+
+  commentDiv.appendChild(commentBlock);
+  commentBlock.appendChild(postedName);
+  commentBlock.appendChild(postedTimeStamp);
+  commentBlock.appendChild(postedDescription);
+}
+
+function timeStamp() {
+  let current = new Date();
+  let cDate = (current.getMonth() + 1) + '/' + current.getDate() + '/' + current.getFullYear();
+  let cTime = current.getHours() + ":" + current.getMinutes() + ":" + current.getSeconds();
+  let dateTime = cDate + ' | ' + cTime;
+  return dateTime;
 }
 
