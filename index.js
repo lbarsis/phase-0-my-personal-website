094f8a3a9caf040d18b871bb0408d286f0305372
Levi Barsis <lbarsis@gmail.com>
2022-06-24 23:18:25 -0700

    Updated form, need to figure out how to submit to Database and use php? Im not sure what the best method is to store comments. I feel this is vital to web development. tomorrow, 6-24-2022, I will start research on how to connect forms to databases.

--- index.js
+++ index.js
@@ -19,6 +19,71 @@ function closeNav() {
 }
 
 function addComment() {
+  const br = document.createElement("br");
+  const commentBlock = document.createElement("div");
+  const commentDiv = document.getElementById("comment-div");
+
+  commentBlock.classList.add("comment-block");
+
+  const form = document.createElement("form");
+  form.classList.add("posted-details");
+  // form.setAttribute("action", "./action_page.php");
+  // form.setAttribute("method", "post");
+
+  // Name section of the form
+  const nameLabel = document.createElement("label");
+  nameLabel.classList.add("posted-details");
+  nameLabel.setAttribute("for", "name");
+  nameLabel.textContent = "Name:";
+
+  const nameInput = document.createElement("input");
+  nameInput.classList.add("posted-details");
+  nameInput.setAttribute("type", "text");
+  nameInput.setAttribute("id", "name");
+
+  // email section of the form
+  const emailLabel = document.createElement("label");
+  emailLabel.classList.add("posted-details");
+  emailLabel.setAttribute("for", "email");
+  emailLabel.textContent = "Email:";
+
+  const emailInput = document.createElement("input");
+  emailInput.classList.add("posted-details");
+  emailInput.setAttribute("type", "text");
+  emailInput.setAttribute("id", "email");
+
+  // comment section of the form
+  const commentLabel = document.createElement("label");
+  commentLabel.classList.add("posted-details");
+  commentLabel.setAttribute("for", "comment");
+  commentLabel.textContent = "Comment:";
+
+  const commentInput = document.createElement("input");
+  commentInput.classList.add("posted-details");
+  commentInput.setAttribute("type", "text");
+  commentInput.setAttribute("id", "comment");
+
+  // Submit Button
+  const submitButton = document.createElement("input");
+  submitButton.classList.add("posted-details");
+  submitButton.setAttribute("type", "submit");
+  submitButton.setAttribute("id", "comment");
+  submitButton.setAttribute("onclick", "postComment()");
+
+  commentDiv.appendChild(commentBlock);
+  commentBlock.appendChild(form);
+  form.appendChild(nameLabel);
+  form.appendChild(nameInput);
+  form.appendChild(br.cloneNode(true));
+  form.appendChild(emailLabel);
+  form.appendChild(emailInput);
+  form.appendChild(br.cloneNode(true));
+  form.appendChild(commentLabel);
+  form.appendChild(commentInput);
+  form.appendChild(submitButton);
+}
+
+function postComment() {
   const commentBlock = document.createElement("div");
   const commentDiv = document.getElementById("comment-div");
 
